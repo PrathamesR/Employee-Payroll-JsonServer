ea0719c75f7b858fcf6e535c85aafb5fba04eaf9
Prathamesh Rajput <rajputprathamesh1998@gmail.com>
2020-11-09 07:28:03 +0530

    [Prathamesh Rajput] Add. Completed Use Case 2

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -30,6 +30,44 @@ namespace EmployeePayrollJsonDB
             Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
 
             Assert.AreEqual(employees.Count, 1);
-        }        
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            HttpStatusCode responseCode = HttpStatusCode.BadRequest;
+            try
+            {
+                string name = "Prathamesh";
+                int Salary = 10000;
+                string startDate = "9/11/2020";
+                decimal PhoneNumber = 123456780;
+                string dept = "IT";
+                string add = "Home";
+                char gender = 'M';
+                string email = "abc@gmail.com";
+
+                RestRequest request = new RestRequest("/employee", Method.POST);
+                JObject JsonContact = new JObject();
+                JsonContact.Add("Name", name);
+                JsonContact.Add("Salary", Salary);
+                JsonContact.Add("StartDate", startDate);
+                JsonContact.Add("PhoneNumber", PhoneNumber);
+                JsonContact.Add("Department", dept);
+                JsonContact.Add("Address", add);
+                JsonContact.Add("Gender", gender);
+                JsonContact.Add("Email", email);
+
+                request.AddParameter("application/json", JsonContact, ParameterType.RequestBody);
+                IRestResponse response = client.Execute(request);
+                responseCode = response.StatusCode;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Assert.AreEqual(responseCode, HttpStatusCode.Created);
+        }
     }
 }
